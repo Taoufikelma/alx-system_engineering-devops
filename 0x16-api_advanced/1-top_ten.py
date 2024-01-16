f0cfa1812bdfb44714987d75675de1be732675a4
taoufik <taoufik@gmail.com>
2024-01-16 12:45:28 -0800

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,13 +8,15 @@ the titles of the first 10 hot posts listed for a given subreddit.
 
 from requests import get
 
+
 def top_ten(subreddit):
     """
    A function that queries the Reddit API and prints
    the titles of the first 10 hot posts listed for a given subreddit.
     """
+
     if subreddit is None or not isinstance(subreddit, str):
-        raise ValueError("Invalid subreddit")
+        print("None")
 
     user_agent = {'User-agent': 'Mozilla/5.0'}
     params = {'limit': 10}
@@ -30,16 +32,4 @@ def top_ten(subreddit):
             print(i.get('data').get('title'))
 
     except Exception:
-        raise ValueError("Unable to retrieve posts")
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        try:
-            top_ten(sys.argv[1])
-        except ValueError as e:
-            print(e)
-
+        print("None")
