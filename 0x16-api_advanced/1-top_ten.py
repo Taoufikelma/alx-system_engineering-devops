628e2f73e81045f35d012964841e9937d4d77af2
taoufik <taoufik@gmail.com>
2024-01-16 12:32:22 -0800

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,36 @@
+
+#!/usr/bin/python3
+
+"""
+A function that queries the Reddit API and prints
+the titles of the first 10 hot posts listed for a given subreddit.
+@authour: Bolakale Aduloju
+"""
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """
+   A function that queries the Reddit API and prints
+   the titles of the first 10 hot posts listed for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
+    user_agent = {'User-agent': 'Mozilla/5.0'}
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    response = get(url, headers=user_agent, params=params)
+    results = response.json()
+
+    try:
+        my_data = results.get('data').get('children')
+
+        for i in my_data:
+            print(i.get('data').get('title'))
+
+    except Exception:
+        print("None")
