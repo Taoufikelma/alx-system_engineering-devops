92e56e07398bdb4141ec95724ef64164fed06fa2
taoufik <taoufik@gmail.com>
2024-02-27 13:24:15 +0000

    stack debb 4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+ky is the limit, let's bring that limit higher
+
+file { '/etc/default/nginx':
+  ensure  => file,
+  content => "ULIMIT=\"-n 10000\"\n",
+}
+
+exec { 'restart nginx':
+  command  => 'service nginx restart',
+  provider => 'shell',
+}
